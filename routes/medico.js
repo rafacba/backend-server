88faeb7145538b936dac25d50b8031af3d26506b
Rafa <rafacbapython@gmail.com>
2021-08-21 21:33:18 -0300

    Seccion 16 - Actualizando hospitales y medicos

--- routes/medico.js
+++ routes/medico.js
@@ -39,6 +39,39 @@ app.get('/', (req, res, next) => {
 });
 
 // ======================================================
+// Obtener medico individual
+// ======================================================
+app.get('/:id', (req, res) => {
+    var id = req.params.id;
+    Medico.findById(id)
+        .populate('usuario', 'nombre email img')
+        .populate('hospital')
+        .exec((err, medico) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar medico',
+                    errors: err
+                });
+            }
+
+            if (!medico) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al buscar el medico',
+                    errors: { message: " No existe un medico con ese ID" }
+                });
+            }
+
+            res.status(200).json({
+                ok: true,
+                medico: medico
+            });
+        });
+});
+
+
+// ======================================================
 // Crear un medico
 // ======================================================
 app.post('/', mdAutenticacion.verificaToken, (req, res) => {
